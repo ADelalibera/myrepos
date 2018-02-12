27c272b26f4e449f1ab4f7b15d6a3395c72a582d
Alexandre Delalibera <delalibera@hotmail.com>
2018-02-12 19:16:31 -0200

    Versão inicial

---
+++ AppTestGit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Versão inicial.");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
