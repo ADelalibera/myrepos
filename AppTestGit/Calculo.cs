586d755fd417b1052a860d04b1053b08d2ec732b
Alexandre Delalibera <delalibera@hotmail.com>
2018-02-12 23:04:30 -0200

    Inclusão de nova classe 1

---
+++ AppTestGit/Calculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTestGit
+{
+    public class Calculo
+    {
+        public int Somar(int x, int y, int z = 0)
+        {
+            return x + y + z;
+        }
+    }
+}
